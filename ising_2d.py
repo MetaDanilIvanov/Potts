408aa72e05ac1049f6407199df00238a472739e1
MetaDanilIvanov <danil.ivanov@metalab.ifmo.ru>
2023-02-28 22:41:56 +0300

    Parallel modeling + timer

--- ising_2d.py
+++ ising_2d.py
@@ -1,19 +1,25 @@
-from __future__ import division
 import numpy as np
 from numpy.random import rand
 import matplotlib.pyplot as plt
+import matplotlib
 import time
+import multiprocessing
+import os
+from datetime import datetime
 
+# wrapped-out model
+# no external field
+# 2d
+# J = 1 interaction
 
-# thanks https://rajeshrinet.github.io/blog/2014/ising-model/
-
-def initialstate(N):
-    ''' generates a random spin configuration for initial condition'''
-    state = 2 * np.random.randint(2, size=(N, N)) - 1
-    return state
+# LaTeX
+matplotlib.rcParams['text.latex.preamble'] = \
+    r'\usepackage{amsmath} \usepackage{amssymb} \usepackage{palatino} \usepackage{textcomp}'
+matplotlib.rcParams['font.family'] = 'sans-serif'
+matplotlib.rcParams['font.sans-serif'] = ['Tahoma']
 
 def mcmove(config, beta):
-    '''Monte Carlo move using Metropolis algorithm '''
+    """Monte Carlo move using Metropolis algorithm """
     for i in range(N):
         for j in range(N):
             a = np.random.randint(0, N)
@@ -31,8 +37,8 @@ def mcmove(config, beta):
 
 
 def calcEnergy(config):
-    '''Energy of a given configuration'''
-    energy = 0
+    """Energy of a given configuration"""
+    energy = np.array(np.zeros(1), dtype=np.longdouble)
     for i in range(len(config)):
         for j in range(len(config)):
             nb = config[(i + 1) % N, j] + config[i, (j + 1) % N] \
@@ -40,83 +46,132 @@ def calcEnergy(config):
             energy += -nb * config[i, j]
     return energy / 4.
 
+def ising_2d(calc, proc):
+    if proc == 0:
+        start = time.time()
+    par = np.zeros(4*calc).reshape((4, calc))
+    for i in range(calc):
+        lists = sim_tt(N, (calc * proc + i))
+        par[0][i] = lists[0]
+        par[1][i] = lists[1]
+        par[2][i] = lists[2]
+        par[3][i] = lists[3]
+        if i == 0 and proc == 0 == 0:
+            print(int(((((time.time() - start)))*
+                   ((100 / (int((1 / calc) * 10000) / 100)) - 1) / 60) * 100) / 100,
+                  'minutes left')
+            print("Current Time =", datetime.now().strftime("%H:%M:%S"))
+    file = open(f"E_{proc}.txt", "w")
+    file.write(str(par[0].tolist()))
+    file.close()
+    file = open(f"M_{proc}.txt", "w")
+    file.write(str(par[1].tolist()))
+    file.close()
+    file = open(f"C_{proc}.txt", "w")
+    file.write(str(par[2].tolist()))
+    file.close()
+    file = open(f"X_{proc}.txt", "w")
+    file.write(str(par[3].tolist()))
+    file.close()
 
-def calcMag(config):
-    '''Magnetization of a given configuration'''
-    mag = np.sum(config)
-    return mag
-
-
-for n in range(1, 3):
-    start_time = time.time()
-    nt = 150  # number of temperature points
-    N = 20  # size of the lattice, N x N
-    eqSteps = 2 ** 6  # number of MC sweeps for equilibration
-    mcSteps = 2 ** 6  # number of MC sweeps for calculation
-
-    T = np.linspace(0.5, 7., nt) #2.3
-    E, M, C, X = np.zeros(nt), np.zeros(nt), np.zeros(nt), np.zeros(nt)
+def sim_tt(N, tt):
     n1, n2 = 1.0 / (mcSteps * N * N), 1.0 / (mcSteps * mcSteps * N * N)
-    ttt = time.time()
-    c = 0
-    between = 0
-    for tt in range(nt):
-        E1 = M1 = E2 = M2 = 0
-        config = initialstate(N)
-        iT = 1.0 / T[tt]
-        iT2 = iT * iT
+    iT = 1.0 / T[tt]
+    E1 = np.array(np.zeros(1), dtype=np.longdouble)
+    M1 = np.array(np.zeros(1), dtype=np.longdouble)
+    E2 = np.array(np.zeros(1), dtype=np.longdouble)
+    M2 = np.array(np.zeros(1), dtype=np.longdouble)
+    config = (2 * np.random.randint(2, size=(N, N)) - 1)
+    for i in range(eqSteps):  # equilibrate
+        mcmove(config, iT)  # Monte Carlo moves
+    for i in range(mcSteps):
+        mcmove(config, iT)
+        Ene = calcEnergy(config)  # calculate the energy
+        Mag = np.sum(config, dtype=np.longdouble)  # calculate the magnetisation
+        E1 += Ene
+        M1 += Mag
+        M2 += (n1 * Mag * Mag)
+        E2 += (n1 * Ene * Ene)
+    return n1 * E1, n1 * M1, (E2 - n2 * E1 * E1) * (iT * iT), (M2 - n2 * M1 * M1) * iT
 
-        for i in range(eqSteps):  # equilibrate
-            mcmove(config, iT)  # Monte Carlo moves
-        for i in range(mcSteps):
-            mcmove(config, iT)
-            Ene = calcEnergy(config)  # calculate the energy
-            Mag = calcMag(config)  # calculate the magnetisation
-            E1 += Ene
-            M1 += Mag
-            M2 += (Mag * Mag)
-            E2 += (Ene * Ene)
+def processesed(procs, calc):
+    processes = []
+    for proc in range(procs):
+        p = multiprocessing.Process(target=ising_2d, args=(calc, proc))
+        processes.append(p)
+        p.start()
+    for p in processes:
+        p.join()
 
-        E[tt] = n1 * E1
-        M[tt] = n1 * M1
-        C[tt] = (n1 * E2 - n2 * E1 * E1) * iT2
-        X[tt] = (n1 * M2 - n2 * M1 * M1) * iT
-        print(int(((tt + 1) / nt) * 10000) / 100, '% of №', n, ' test', sep='')
-        if c == 0:
-            c += 1
-            between = time.time()
-        print('Left:',
-              int((((int(((between - ttt)) * 1000) / 1000)*
-              ((100/(int((1 / nt) * 10000) / 100))-tt)
-              )/60)*100)/100,
-              'mins')
-    f = plt.figure(figsize=(18, 10))  # plot the calculated values
+# 2d
+n_proc = multiprocessing.cpu_count()
+it = 300
+calc = it // n_proc + ((it // n_proc) != (it / n_proc))
+nt = int(calc * n_proc)  # number of temperature points
+eqSteps = 2 ** 10  # number of MC sweeps for equilibration
+mcSteps = 2 ** 10  # number of MC sweeps for calculation
+N = 50  # size of lattice
+T = np.linspace(1.5, 3.5, nt)  # 2.25
+if __name__ == "__main__":
+    Start = time.time()
+    processesed(n_proc, calc)
+    E = []
+    M = []
+    C = []
+    X = []
+    E_list = []
+    M_list = []
+    C_list = []
+    X_list = []
+    for i in range(n_proc):
+        E_list.append(f"E_{i}.txt")
+        M_list.append(f"M_{i}.txt")
+        C_list.append(f"C_{i}.txt")
+        X_list.append(f"X_{i}.txt")
+    for i in range(n_proc):
+        with open(f"E_{i}.txt", "r") as f:
+            E.append(eval(f.readline()))
+        with open(f"M_{i}.txt", "r") as f:
+            M.append(eval(f.readline()))
+        with open(f"C_{i}.txt", "r") as f:
+            C.append(eval(f.readline()))
+        with open(f"X_{i}.txt", "r") as f:
+            X.append(eval(f.readline()))
+    E = [a for b in E for a in b]
+    M = np.array([a for b in M for a in b])
+    C = [a for b in C for a in b]
+    X = [a for b in X for a in b]
+    for i in range(n_proc):
+        os.remove(f"E_{i}.txt")
+        os.remove(f"M_{i}.txt")
+        os.remove(f"C_{i}.txt")
+        os.remove(f"X_{i}.txt")
+
+    f = plt.figure(figsize=(18, 10))
 
     sp1 = f.add_subplot(2, 2, 1)
-    plt.scatter(T, E, s=50, marker='o', color='IndianRed')
-    plt.xlabel("Temperature (T)", fontsize=20)
-    plt.ylabel("Energy ", fontsize=20)
+    plt.scatter(T, E, s=3, marker='o', color='IndianRed')
+    plt.xlabel("$T$", fontsize=25)
+    plt.ylabel("$E$", fontsize=25)
     plt.axis('tight')
 
     sp2 = f.add_subplot(2, 2, 2)
-    plt.scatter(T, abs(M), s=50, marker='o', color='RoyalBlue')
-    plt.xlabel("Temperature (T)", fontsize=20)
-    plt.ylabel("Magnetization ", fontsize=20)
+    plt.scatter(T, abs(M), s=3, marker='o', color='RoyalBlue')
+    plt.xlabel("$T$", fontsize=25)
+    plt.ylabel("$M$ ", fontsize=25)
     plt.axis('tight')
 
     sp3 = f.add_subplot(2, 2, 3)
-    plt.scatter(T, C, s=50, marker='o', color='IndianRed')
-    plt.xlabel("Temperature (T)", fontsize=20)
-    plt.ylabel("Specific Heat ", fontsize=20)
+    plt.scatter(T, C, s=3, marker='o', color='IndianRed')
+    plt.xlabel("$T$", fontsize=25)
+    plt.ylabel("$C_v$", fontsize=25)
     plt.axis('tight')
 
     sp4 = f.add_subplot(2, 2, 4)
-    plt.scatter(T, X, s=50, marker='o', color='RoyalBlue')
-    plt.xlabel("Temperature (T)", fontsize=20)
-    plt.ylabel("Susceptibility", fontsize=20)
+    plt.scatter(T, X, s=3, marker='o', color='RoyalBlue')
+    plt.xlabel("$T$", fontsize=25)
+    plt.ylabel("$\chi$", fontsize=25)
     plt.axis('tight')
-    name = 'ising_2d_MC_test' + str(n) + '.png'
-    plt.savefig(name, bbox_inches='tight', dpi=800)
-    print('№', n, ' test completed', sep='')
-    print("--- %s minutes ---" % (int(((time.time() - start_time) / 60) * 1000) / 1000))
-    break
+
+    plt.savefig(f'ising_2d_MC_test_N={str(N)}.png', bbox_inches='tight', dpi=500)
+    print(f'total time {int(((time.time() - Start)*1000)/60)/1000} minutes')
--- ising_3d.py
+++ ising_3d.py
@@ -4,26 +4,19 @@ import matplotlib.pyplot as plt
 import matplotlib
 import time
 import multiprocessing
+import os
+from datetime import datetime
 
 # wrapped-out model
 # no external field
 # 3d
 # J = 1 interaction
+
 # LaTeX
 matplotlib.rcParams['text.latex.preamble'] = \
     r'\usepackage{amsmath} \usepackage{amssymb} \usepackage{palatino} \usepackage{textcomp}'
 matplotlib.rcParams['font.family'] = 'sans-serif'
 matplotlib.rcParams['font.sans-serif'] = ['Tahoma']
-
-
-def initial_state(N: int):
-    """generates a random spin configuration for initial condition 3d"""
-    state = []
-    for levels in range(N):
-        slicee = 2 * np.random.randint(2, size=(N, N)) - 1
-        state.append(slicee.tolist())
-    return np.array(state)
-
 
 def mcmove(config, beta):
     """Monte Carlo move using Metropolis algorithm """
@@ -45,10 +38,9 @@ def mcmove(config, beta):
                 config[a, b, c] = s
     return config
 
-
 def calcEnergy(config):
     """Energy of a given configuration"""
-    energy = 0
+    energy = np.array(np.zeros(1), dtype=np.longdouble)
     for i in range(len(config)):
         for j in range(len(config)):
             for k in range(len(config)):
@@ -57,70 +49,54 @@ def calcEnergy(config):
                      + config[i, j, (k + 1) % N] + config[i, j, (k - 1) % N]
                 energy += -1 * nb * config[i, j, k]
     return energy / 6.
-
-
-def calcMag(config):
-    """Magnetization of a given configuration"""
-    mag = np.sum(config)
-    return mag
-
-
 def ising_3d(calc, proc):
     if proc == 0:
         start = time.time()
-    Eeee = np.zeros(calc)
-    Mmmm = np.zeros(calc)
-    Cccc = np.zeros(calc)
-    Xxxx = np.zeros(calc)
+    par = np.zeros(4*calc).reshape((4, calc))
     for i in range(calc):
         lists = sim_tt(N, (calc * proc + i))
-        Eeee[i] = sum(lists[0])
-        Mmmm[i] = sum(lists[1])
-        Cccc[i] = sum(lists[2])
-        Xxxx[i] = sum(lists[3])
-        if i == 0 and proc == 0:
-            print(int(((((((time.time() - start)))*
-                   ((100 / (int((1 / calc) * 10000) / 100)) - 1) / 60) * 100) / 100)*1000)/1000,
+        par[0][i] = lists[0]
+        par[1][i] = lists[1]
+        par[2][i] = lists[2]
+        par[3][i] = lists[3]
+        if i == 0 and proc == 0 == 0:
+            print(int(((((time.time() - start)))*
+                   ((100 / (int((1 / calc) * 10000) / 100)) - 1) / 60) * 100) / 100,
                   'minutes left')
-
-    file = open("E_%s.txt" % proc, "w")
-    file.write(str(Eeee.tolist()))
+            print("Current Time =", datetime.now().strftime("%H:%M:%S"))
+    file = open(f"E_{proc}.txt", "w")
+    file.write(str(par[0].tolist()))
     file.close()
-    file = open("M_%s.txt" % proc, "w")
-    file.write(str(Mmmm.tolist()))
+    file = open(f"M_{proc}.txt", "w")
+    file.write(str(par[1].tolist()))
     file.close()
-    file = open("C_%s.txt" % proc, "w")
-    file.write(str(Cccc.tolist()))
+    file = open(f"C_{proc}.txt", "w")
+    file.write(str(par[2].tolist()))
     file.close()
-    file = open("X_%s.txt" % proc, "w")
-    file.write(str(Xxxx.tolist()))
+    file = open(f"X_{proc}.txt", "w")
+    file.write(str(par[3].tolist()))
     file.close()
-
 
 
 def sim_tt(N, tt):
     n1, n2 = 1.0 / (mcSteps * N * N), 1.0 / (mcSteps * mcSteps * N * N)
     iT = 1.0 / T[tt]
-    E1 = np.array(np.zeros(1), dtype=np.float64)
-    M1 = np.array(np.zeros(1), dtype=np.float64)
-    E2 = np.array(np.zeros(1), dtype=np.float64)
+    E1 = np.array(np.zeros(1), dtype=np.longdouble)
+    M1 = np.array(np.zeros(1), dtype=np.longdouble)
+    E2 = np.array(np.zeros(1), dtype=np.longdouble)
     M2 = np.array(np.zeros(1), dtype=np.longdouble)
-    config = initial_state(N)
+    config = (2 * np.random.randint(2, size=(N, N, N)) - 1)
     for i in range(eqSteps):  # equilibrate
         mcmove(config, iT)  # Monte Carlo moves
     for i in range(mcSteps):
         mcmove(config, iT)
         Ene = calcEnergy(config)  # calculate the energy
-        Mag = calcMag(config)  # calculate the magnetisation
+        Mag = np.sum(config, dtype=np.longdouble)  # calculate the magnetisation
         E1 += Ene
         M1 += Mag
-        M2 += (Mag * Mag)
-        E2 += (Ene * Ene)
-    Ee = n1 * E1
-    Mm = n1 * M1
-    Cc = (n1 * E2 - n2 * E1 * E1) * (iT * iT)
-    Xx = (n1 * M2 - n2 * M1 * M1) * iT
-    return Ee, Mm, Cc, Xx
+        M2 += (n1 * Mag * Mag)
+        E2 += (n1 * Ene * Ene)
+    return n1 * E1, n1 * M1, (E2 - n2 * E1 * E1) * (iT * iT), (M2 - n2 * M1 * M1) * iT
 
 
 def processesed(procs, calc):
@@ -132,16 +108,17 @@ def processesed(procs, calc):
     for p in processes:
         p.join()
 
-
+# 3d
 n_proc = multiprocessing.cpu_count()
-it = 200
+it = 300
 calc = it // n_proc + ((it // n_proc) != (it / n_proc))
 nt = int(calc * n_proc)  # number of temperature points
-eqSteps = 2 ** 8  # number of MC sweeps for equilibration
-mcSteps = 2 ** 8  # number of MC sweeps for calculation
-N = 20  # size of lattice
-T = np.linspace(2.3, 6., nt)  # 4.5
+eqSteps = 2 ** 9  # number of MC sweeps for equilibration
+mcSteps = 2 ** 9  # number of MC sweeps for calculation
+N = 10  # size of lattice
+T = np.linspace(2.5, 5.5, nt)  # 4.5
 if __name__ == "__main__":
+    Start = time.time()
     processesed(n_proc, calc)
     E = []
     M = []
@@ -152,49 +129,54 @@ if __name__ == "__main__":
     C_list = []
     X_list = []
     for i in range(n_proc):
-        E_list.append("E_%s.txt" % i)
-        M_list.append("M_%s.txt" % i)
-        C_list.append("C_%s.txt" % i)
-        X_list.append("X_%s.txt" % i)
+        E_list.append(f"E_{i}.txt")
+        M_list.append(f"M_{i}.txt")
+        C_list.append(f"C_{i}.txt")
+        X_list.append(f"X_{i}.txt")
     for i in range(n_proc):
-        with open(str(E_list[i]), "r") as f:
+        with open(f"E_{i}.txt", "r") as f:
             E.append(eval(f.readline()))
-        with open(str(M_list[i]), "r") as f:
+        with open(f"M_{i}.txt", "r") as f:
             M.append(eval(f.readline()))
-        with open(str(C_list[i]), "r") as f:
+        with open(f"C_{i}.txt", "r") as f:
             C.append(eval(f.readline()))
-        with open(str(X_list[i]), "r") as f:
+        with open(f"X_{i}.txt", "r") as f:
             X.append(eval(f.readline()))
     E = [a for b in E for a in b]
     M = np.array([a for b in M for a in b])
     C = [a for b in C for a in b]
     X = [a for b in X for a in b]
+    for i in range(n_proc):
+        os.remove(f"E_{i}.txt")
+        os.remove(f"M_{i}.txt")
+        os.remove(f"C_{i}.txt")
+        os.remove(f"X_{i}.txt")
 
     f = plt.figure(figsize=(18, 10))
 
     sp1 = f.add_subplot(2, 2, 1)
-    plt.scatter(T, E, s=20, marker='o', color='IndianRed')
-    plt.xlabel("$T$", fontsize=10)
-    plt.ylabel("E", fontsize=10)
+    plt.scatter(T, E, s=3, marker='o', color='IndianRed')
+    plt.xlabel("$T$", fontsize=25)
+    plt.ylabel("$E$", fontsize=25)
     plt.axis('tight')
 
     sp2 = f.add_subplot(2, 2, 2)
-    plt.scatter(T, abs(M), s=20, marker='o', color='RoyalBlue')
-    plt.xlabel("$T$", fontsize=10)
-    plt.ylabel("M ", fontsize=10)
+    plt.scatter(T, abs(M), s=3, marker='o', color='RoyalBlue')
+    plt.xlabel("$T$", fontsize=25)
+    plt.ylabel("$M$ ", fontsize=25)
     plt.axis('tight')
 
     sp3 = f.add_subplot(2, 2, 3)
-    plt.scatter(T, C, s=20, marker='o', color='IndianRed')
-    plt.xlabel("$T$", fontsize=10)
-    plt.ylabel("$C_v$", fontsize=10)
+    plt.scatter(T, C, s=3, marker='o', color='IndianRed')
+    plt.xlabel("$T$", fontsize=25)
+    plt.ylabel("$C_v$", fontsize=25)
     plt.axis('tight')
 
     sp4 = f.add_subplot(2, 2, 4)
-    plt.scatter(T, X, s=20, marker='o', color='RoyalBlue')
-    plt.xlabel("$T$", fontsize=10)
-    plt.ylabel("$\chi$", fontsize=10)
+    plt.scatter(T, X, s=3, marker='o', color='RoyalBlue')
+    plt.xlabel("$T$", fontsize=25)
+    plt.ylabel("$\chi$", fontsize=25)
     plt.axis('tight')
 
-    name = 'ising_3d_MC_test_N=' + str(N) + '.png'
-    plt.savefig(name, bbox_inches='tight', dpi=500)
+    plt.savefig(f'ising_3d_MC_test_N={str(N)}.png', bbox_inches='tight', dpi=500)
+    print(f'total time {int(((time.time() - Start)*1000)/60)/1000} minutes')
